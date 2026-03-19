0a2adbbda19dbf283263d290dbe0e307b7f47af2
jade <lenguyenngoc2006qb@gmail.com>
2026-03-19 09:22:57 +0700

    Delete GolfDaPhuoc-main directory

--- GolfDaPhuoc-main/text/principle.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-    '- Hoạt động từ thiện, phi lợi nhuận',
-    '- Giúp học sinh, sinh viên có hoàn cảnh khó khăn, thực sự có nhu cầu hỗ trợ, có tinh thần vượt khó, có ý chí vươn lên, có khả năng phát triển trong tương lai',
-    '- Góp phần xây dựng những tài năng cho thành phố và đất nước',
-    '- Tạo nguồn nhân lực cho Câu lạc bộ trong tương lai'
-];
-?>
